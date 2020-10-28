7001a79d724d5279c83bfab1ddd0b7f5a5c160cf
Arnau Sanchez <pyarnau@gmail.com>
2020-10-28 20:21:42 +0100

    Add config entity + repo + usecase

---
+++ src/data/Dhis2ConfigRepository.ts
@@ -0,0 +1,23 @@
+import { ConfigRepository } from "../domain/repositories/ConfigRepository";
+import { Config } from "../domain/entities/Config";
+import { D2Api } from "../types/d2-api";
+import { keyById } from "../domain/entities/Base";
+
+export class Dhis2ConfigRepository implements ConfigRepository {
+    constructor(private api: D2Api) {}
+
+    async get(): Promise<Config> {
+        const toName = { $fn: { name: "rename", to: "name" } } as const;
+        const res$ = this.api.metadata.get({
+            dataSets: {
+                fields: { id: true, displayName: toName },
+                filter: { name: { ilike: "NHWA Module" } },
+            },
+        });
+
+        const { dataSets } = await res$.getData();
+        return {
+            dataSets: keyById(dataSets),
+        };
+    }
+}
