796f71dba84046c9a1381465aa43da631b9eafab
Arnau Sanchez <pyarnau@gmail.com>
2020-10-29 15:11:28 +0100

    Refactor useSnackbarOnError

--- src/webapp/utils/snackbar.ts
+++ src/webapp/utils/snackbar.ts
@@ -1,30 +1,21 @@
+import React from "react";
 import _ from "lodash";
-import { SnackbarState } from "d2-ui-components";
+import { useSnackbar } from "d2-ui-components";
 
-interface Options<T> {
-    onFinally?(): void;
-    onCatch?(): void;
-}
+export function useSnackbarOnError<T>(fn: () => Promise<T>) {
+    const snackbar = useSnackbar();
 
-export async function useSnackbarOnError<T>(
-    snackbar: SnackbarState,
-    fn: () => Promise<T>,
-    options?: Options<T>
-): Promise<T | undefined> {
-    const { onCatch, onFinally } = options || {};
-    try {
-        const res = await fn();
-        return res;
-    } catch (err) {
-        const bodyMessage = err.response?.data?.message;
-        console.error(err);
-        if (onCatch) onCatch();
-        const message = _([err.message || err?.toString(), bodyMessage])
-            .compact()
-            .join(" - ");
-        snackbar.error(message);
-        return undefined;
-    } finally {
-        if (onFinally) onFinally();
-    }
+    return React.useCallback(async () => {
+        try {
+            return await fn();
+        } catch (err) {
+            const bodyMessage = err.response?.data?.message;
+            console.error(err);
+            const message = _([err.message || err?.toString(), bodyMessage])
+                .compact()
+                .join(" - ");
+            snackbar.error(message);
+            return undefined;
+        }
+    }, [fn, snackbar]);
 }
