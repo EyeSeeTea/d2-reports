df72c8c6f52e6094cd495f3b634c55d954b1b123
nshandra <34254522+nshandra@users.noreply.github.com>
2022-09-12 18:25:44 +0200

    Add one-time generateSortOrder() invocation.

--- src/webapp/reports/mal-dataset-duplication/MALDataApprovalStatusReport.tsx
+++ src/webapp/reports/mal-dataset-duplication/MALDataApprovalStatusReport.tsx
@@ -1,9 +1,16 @@
 import { Typography, makeStyles } from "@material-ui/core";
+import { useEffect } from "react";
 import i18n from "../../../locales";
+import { useAppContext } from "../../contexts/app-context";
 import { DataApprovalList } from "./data-approval-list/DataApprovalList";
 
 const MALDataApprovalStatusReport: React.FC = () => {
     const classes = useStyles();
+    const { compositionRoot } = useAppContext();
+
+    useEffect(() => {
+        compositionRoot.dataDuplicate.generateSortOrder()
+    });
 
     return (
         <div className={classes.wrapper}>
