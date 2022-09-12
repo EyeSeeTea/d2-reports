4328c81492b16bd443ba809ab6f418eb444986da
nshandra <34254522+nshandra@users.noreply.github.com>
2022-09-12 18:18:21 +0200

    Add getSortOrder and generateSortOrder.

--- src/data/MALDataDuplicationDefaultRepository.ts
+++ src/data/MALDataDuplicationDefaultRepository.ts
@@ -432,6 +432,49 @@ export class MALDataDuplicationDefaultRepository implements MALDataDuplicationRe
     async saveColumns(namespace: string, columns: string[]): Promise<void> {
         return this.storageClient.saveObject<string[]>(namespace, columns);
     }
+
+    async getSortOrder(): Promise<string[]> {
+        const sortOrderArray = await this.storageClient.getObject<string[]>(Namespaces.MAL_DIFF_NAMES_SORT_ORDER);
+
+        return sortOrderArray ?? [];
+    }
+
+    async generateSortOrder(): Promise<void> {
+        try {
+            const dataSetData: {
+                dataSetElements: { dataElement: { id: string, name: string } }[],
+                sections: { id: string }[]
+            } = await this.api.get<any>(
+                `/dataSets/PWCUb3Se1Ie`,
+                { fields: "sections,dataSetElements[dataElement[id,name]]" }
+            ).getData();
+
+            const dataSetElements: { id: string, name: string }[] = dataSetData.dataSetElements.map((item) =>
+                (item.dataElement)
+            );
+
+            const sectionsDEs = await promiseMap(dataSetData.sections, async sections => {
+                return this.api.get<any>(
+                    `/sections/${sections.id}`,
+                    { fields: "dataElements" }
+                ).getData()
+            });
+
+            const sectionsDEsIds: { id: string }[] = sectionsDEs.flatMap((item) => {
+                return item.dataElements.map((dataElementId: { id: string }) => {
+                    return dataElementId;
+                })
+            });
+
+            const sortOrderArray: string[] = sectionsDEsIds.map(obj =>
+                Object.assign(obj, dataSetElements.find((obj2) => obj.id === obj2.id))
+            ).map(item => (item.name));
+
+            return this.storageClient.saveObject<string[]>(Namespaces.MAL_DIFF_NAMES_SORT_ORDER, sortOrderArray);
+        } catch (error: any) {
+            console.debug(error);
+        }
+    }
 }
 
 const csvFields = ["dataSet", "orgUnit", "period", "completed"] as const;
