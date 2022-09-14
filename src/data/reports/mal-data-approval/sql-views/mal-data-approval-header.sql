1560a723109a165f0c0c8198e6c7c69a18d378a4
idelcano <ignaciodelcano@gmail.com>
2022-09-14 20:54:31 +0200

    added missing sqlviews

---
+++ src/data/reports/mal-data-approval/sql-views/mal-data-approval-header.sql
@@ -0,0 +1,9 @@
+SELECT dataset.name                                          AS dataset,
+       dataset.uid                                                       AS datasetuid,
+       organisationunit.uid                                       AS orgunituid,
+       organisationunit.name                                  AS orgunit
+       FROM dataset
+                JOIN datasetsource USING (datasetid)
+                JOIN organisationunit ON (organisationunitid=sourceid)
+where dataset.uid    ~ ('^' || replace('${dataSets}', '-', '|') || '$')
+group by dataset.uid, dataset.name, organisationunit.name, organisationunit.uid
