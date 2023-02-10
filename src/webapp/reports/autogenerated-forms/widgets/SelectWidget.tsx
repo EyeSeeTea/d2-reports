22773f7936776265e4316fcfc0f3a70c70ceb10f
Arnau Sanchez <pyarnau@gmail.com>
2023-02-10 13:58:19 +0100

    Add placeholder to option widget

--- src/webapp/reports/autogenerated-forms/widgets/SelectWidget.tsx
+++ src/webapp/reports/autogenerated-forms/widgets/SelectWidget.tsx
@@ -4,6 +4,7 @@ import { SingleSelect, SingleSelectOption } from "@dhis2/ui";
 import { Option } from "../../../../domain/common/entities/DataForm";
 import { Maybe } from "../../../../utils/ts-utils";
 import { WidgetFeedback, WidgetState } from "../WidgetFeedback";
+import i18n from "@eyeseetea/d2-ui-components/locales";
 
 export interface SelectWidgetProps {
     value: Maybe<string>;
@@ -30,7 +31,12 @@ const SelectWidget: React.FC<SelectWidgetProps> = props => {
 
     return (
         <WidgetFeedback state={props.state}>
-            <SingleSelect onChange={notifyChange} selected={stateValue} disabled={disabled}>
+            <SingleSelect
+                onChange={notifyChange}
+                selected={stateValue}
+                disabled={disabled}
+                placeholder={i18n.t("Select option")}
+            >
                 {options.map(({ id, name, code }) => (
                     <SingleSelectOption key={`option-${id}`} label={name} value={code} />
                 ))}
