939d7eb7f354eac93bc11e6ca1589d67836cce41
Arnau Sanchez <pyarnau@gmail.com>
2023-02-10 11:43:53 +0100

    Add build for autogenerate forms

---
+++ src/scripts/common.ts
@@ -0,0 +1,8 @@
+import { execSync } from "child_process";
+
+export function build(variant: string): void {
+    Object.assign(process.env, { REACT_APP_REPORT_VARIANT: variant });
+    const cmd = `yarn build`;
+    console.debug(`Run: ${cmd}`);
+    execSync(cmd, { stdio: [0, 1, 2] });
+}
