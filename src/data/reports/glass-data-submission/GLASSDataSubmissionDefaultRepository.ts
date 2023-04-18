615a1860a3103d9f29c576954f771de6c79ec7fa
deeonwuli <onwulidumebi@gmail.com>
2023-04-18 11:00:48 +0100

    fix: remove duplicates

--- src/data/reports/glass-data-submission/GLASSDataSubmissionDefaultRepository.ts
+++ src/data/reports/glass-data-submission/GLASSDataSubmissionDefaultRepository.ts
@@ -111,14 +111,11 @@ export class GLASSDataSubmissionDefaultRepository implements GLASSDataSubmission
             }
             if (validatedDatasets > 0) {
                 dataSetsUploaded += `${validatedDatasets} validated, `;
-                dataSetsUploaded += `${validatedDatasets} validated, `;
             }
             if (importedDatasets > 0) {
                 dataSetsUploaded += `${importedDatasets} imported, `;
-                dataSetsUploaded += `${importedDatasets} imported, `;
             }
             if (uploadedDatasets > 0) {
-                dataSetsUploaded += `${uploadedDatasets} uploaded, `;
                 dataSetsUploaded += `${uploadedDatasets} uploaded, `;
             }
 
