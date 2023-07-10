49b616226645625af68dda8882e97730132bf351
deeonwuli <onwulidumebi@gmail.com>
2023-07-10 13:59:37 +0100

    fix: get correct report type

--- src/data/common/clients/storage/Namespaces.ts
+++ src/data/common/clients/storage/Namespaces.ts
@@ -10,15 +10,12 @@ export const Namespaces = {
     MAL_DIFF_STATUS_USER_COLUMNS: "mal-diff-status-user-columns",
     MAL_DIFF_NAMES_SORT_ORDER: "mal-diff-names-sort-order",
     MONITORING: "monitoring",
-<<<<<<< HEAD
     MAL_SUBSCRIPTION_STATUS_USER_COLUMNS: "mal-subscription-status-user-columns",
     MAL_SUBSCRIPTION_STATUS: "mal-subscription-status",
-=======
     DATA_SUBMISSSIONS: "data-submissions",
     DATA_SUBMISSSIONS_USER_COLUMNS: "data-submissions-user-columns",
     DATA_SUBMISSSIONS_MODULES: "modules",
     DATA_SUBMISSSIONS_UPLOADS: "uploads",
->>>>>>> development
 };
 
 export const NamespaceProperties: Record<Namespace, string[]> = {
@@ -27,12 +24,9 @@ export const NamespaceProperties: Record<Namespace, string[]> = {
     [Namespaces.MAL_DIFF_STATUS_USER_COLUMNS]: [],
     [Namespaces.MAL_DIFF_NAMES_SORT_ORDER]: [],
     [Namespaces.MONITORING]: [],
-<<<<<<< HEAD
     [Namespaces.MAL_SUBSCRIPTION_STATUS]: [],
-=======
     [Namespaces.DATA_SUBMISSSIONS]: [],
     [Namespaces.DATA_SUBMISSSIONS_USER_COLUMNS]: [],
     [Namespaces.DATA_SUBMISSSIONS_MODULES]: [],
     [Namespaces.DATA_SUBMISSSIONS_UPLOADS]: [],
->>>>>>> development
 };
