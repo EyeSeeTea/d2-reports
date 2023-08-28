3e2c9bc12ef9a642a59ca55605be735ae65a15e1
deeonwuli <onwulidumebi@gmail.com>
2023-08-28 12:17:42 +0100

    fix: remove build subscrption script
    fix build errors

--- src/data/reports/mal-data-subscription/sql-views/mal-approval-dataelements.sql
+++
@@ -1,5 +0,0 @@
-select de.uid as dataelementuid, de.name as dataelementname, se.name as sectionname,  se.uid as sectionuid,
-(select max(lastupdated) from datavalue where dataelementid=de.dataelementid) as lastdateofsubscription 
-from section se inner join sectiondataelements using (sectionid)
-inner join dataelement de using(dataelementid) 
-where datasetid in (select datasetid from dataset where uid  ~ ('^' || replace('${dataSets}', '-', '|') || '$'))
