a7a53d524694a5482ca7431cd3cf122f7659eee7
deeonwuli <onwulidumebi@gmail.com>
2023-08-28 22:32:28 +0100

    fix: make monitoring dataset object an array

--- src/domain/reports/mal-data-approval/entities/MalDataApprovalItem.ts
+++ src/domain/reports/mal-data-approval/entities/MalDataApprovalItem.ts
@@ -39,7 +39,13 @@ export interface CountryCode {
     code: string;
 }
 
-export type MonitoringValue = Record<string, Record<string, { monitoring: Monitoring[]; userGroups: string }>>;
+export type MonitoringValue = Record<
+    string,
+    Record<
+        string,
+        { monitoring: Monitoring[]; userGroups: string }[] | { monitoring: Monitoring[]; userGroups: string }[]
+    >
+>;
 
 export function getDataDuplicationItemId(dataSet: MalDataApprovalItem): string {
     return [
@@ -64,7 +70,8 @@ export function getDataDuplicationItemMonitoringValue(
             )?.monitoring ?? false
         );
     } else {
-        const monitoringArray = monitoring["dataSets"]?.[dataSetName]?.monitoring;
+        const monitoringArray = _.first(monitoring["dataSets"]?.[dataSetName])?.monitoring;
+
         return !!_.find(monitoringArray, { orgUnit: dataSet.orgUnitCode, period: dataSet.period });
     }
 }
