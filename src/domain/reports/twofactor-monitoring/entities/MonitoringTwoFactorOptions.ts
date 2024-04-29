99d536d98e4864680dcc1b771062a8aaec27873b
idelcano <ignaciodelcano@gmail.com>
2024-04-29 20:15:43 +0200

    Fix and refactor twofactor monitoring domain report classes

--- src/domain/reports/twofactor-monitoring/entities/MonitoringTwoFactorOptions.ts
+++ src/domain/reports/twofactor-monitoring/entities/MonitoringTwoFactorOptions.ts
@@ -1,12 +1,9 @@
-import { Config } from "../../../common/entities/Config";
 import { Paging, Sorting } from "../../../common/entities/PaginatedObjects";
 import { MonitoringTwoFactorUser } from "./MonitoringTwoFactorUser";
 
 export interface MonitoringTwoFactorOptions {
-    config: Config;
     paging: Paging;
     sorting: Sorting<MonitoringTwoFactorUser>;
-    disabled: boolean;
     usernameQuery: string;
-    uidQuery: string;
+    groups: string[];
 }
--- src/domain/reports/twofactor-monitoring/repositories/MonitoringTwoFactorRepository.ts
+++ src/domain/reports/twofactor-monitoring/repositories/MonitoringTwoFactorRepository.ts
@@ -1,9 +1,12 @@
-import { PaginatedObjects } from "../../../common/entities/PaginatedObjects";
 import { MonitoringTwoFactorOptions } from "../entities/MonitoringTwoFactorOptions";
+import { MonitoringTwoFactorPaginatedObjects } from "../entities/MonitoringTwoFactorPaginatedObjects";
 import { MonitoringTwoFactorUser } from "../entities/MonitoringTwoFactorUser";
 
 export interface MonitoringTwoFactorRepository {
-    get(namespace: string, options: MonitoringTwoFactorOptions): Promise<MonitoringTwoFactorUser[]>;
+    get(
+        namespace: string,
+        options: MonitoringTwoFactorOptions
+    ): Promise<MonitoringTwoFactorPaginatedObjects<MonitoringTwoFactorUser>>;
     save(fileName: string, items: MonitoringTwoFactorUser[]): Promise<void>;
     getColumns(namespace: string): Promise<string[]>;
     saveColumns(namespace: string, columns: string[]): Promise<void>;
