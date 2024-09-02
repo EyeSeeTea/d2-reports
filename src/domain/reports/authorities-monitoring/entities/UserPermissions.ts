c3b1839b07b1917cd5773d3a647769a72461d373
idelcano <ignaciodelcano@gmail.com>
2024-09-02 12:48:40 +0200

    Remove unnecesary methods and rename isTemplateUser to some more readable

--- src/domain/reports/authorities-monitoring/entities/UserPermissions.ts
+++ src/domain/reports/authorities-monitoring/entities/UserPermissions.ts
@@ -1,4 +1,3 @@
-import _ from "lodash";
 import { NamedRef, Ref } from "../../../common/entities/Base";
 import { UserMonitoring } from "../../../../data/reports/authorities-monitoring/AuthoritiesMonitoringDefaultRepository";
 
@@ -65,46 +64,10 @@ export class UserPermissions {
         return excludedUsers.some(excludedUser => this.user.id === excludedUser.id);
     }
 
-    public hasExcludedRole(): boolean {
-        const { excludedRoles } = this.userMonitoring;
-        const excludedRoleIds = excludedRoles.map(role => role.id);
-        const userRoleIds = this.user.userCredentials.userRoles.map(role => role.id);
-
-        return _.intersection(excludedRoleIds, userRoleIds).length > 0;
-    }
-
-    public isTemplateUser(): boolean {
+    public hasAssignedTemplateGroup(): boolean {
         const { templates } = this.userMonitoring;
         const userTemplateIds = templates.map(template => template.template.id);
 
         return userTemplateIds.includes(this.user.id);
-    }
-
-    public isExcludedByRolesByGroup(): boolean {
-        const { excludedRolesByGroup } = this.userMonitoring;
-
-        return excludedRolesByGroup.some(excludedRolesByGroup => {
-            const isInExcludedUserGroup = this.user.userGroups
-                .map(userGroup => userGroup.id)
-                .includes(excludedRolesByGroup.group.id);
-            const hasExcludedRole = this.user.userCredentials.userRoles
-                .map(userRole => userRole.id)
-                .includes(excludedRolesByGroup.role.id);
-
-            return isInExcludedUserGroup && hasExcludedRole;
-        });
-    }
-
-    public isExcludedByRolesByUsers(): boolean {
-        const { excludedRolesByUser } = this.userMonitoring;
-        const userRoleIds = this.user.userCredentials.userRoles.map(role => role.id);
-
-        const isExcludedByRole = this.user.roles.map(role => role.id).some(role => userRoleIds.includes(role));
-        const isExcludedByRoleByUsers = excludedRolesByUser
-            .map(roleByUser => roleByUser.role.id)
-            .some(role => userRoleIds.includes(role));
-        const isExcludedUser = excludedRolesByUser.some(user => user.user.id === this.user.id);
-
-        return !isExcludedByRole && isExcludedByRoleByUsers && isExcludedUser;
     }
 }
