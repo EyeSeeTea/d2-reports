f28b529184482208ecf564eed9e2dd7a7196914e
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2024-09-17 17:57:10 +0100

    fix: remove duplicate getEventQueryString function

--- src/domain/common/entities/AuditAnalyticsResponse.ts
+++ src/domain/common/entities/AuditAnalyticsResponse.ts
@@ -36,15 +36,3 @@ export class AuditAnalyticsData {
 export function buildRefs(ids: string[]): AuditItem[] {
     return ids.map(id => ({ registerId: id }));
 }
-
-export function getEventQueryString(
-    programId: string,
-    programStageId: string,
-    orgUnitIds: string,
-    period: string,
-    query: string
-) {
-    const eventQueryString = `/analytics/events/query/${programId}.json?dimension=pe:${period}&dimension=ou:${orgUnitIds}&stage=${programStageId}${query}&pageSize=100000`;
-
-    return eventQueryString;
-}
